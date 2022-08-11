3724af7d19cc0cda2d8771940b4a8a6fae254dd2
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-08-11 21:15:59 +0500

    fix: css in main and blockOne

--- src/Components/BlockOne/BlockOne.js
+++ src/Components/BlockOne/BlockOne.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './BlockOne.module.css'
+import {TbPoint} from "react-icons/tb";
 
 export const BlockOne = () => {
     return (
@@ -11,27 +12,32 @@ export const BlockOne = () => {
             </div>
             <div className={style.block2}>
                 <div className={style.item}>
-                    <span>
+                    <TbPoint color='white' size='1.3em'/>
+                    <span className={style.itemText}>
                     Добаюкивание - это дополучение родительской любви, признания, физической ласки во взрослом возрасте.
                 </span>
                 </div>
                 <div className={style.item}>
-                    <span>
+                    <TbPoint color='white' size='1.3em'/>
+                    <span className={style.itemText}>
                     Взрослый человек получает возможность добрать то, что не смог получить в детстве. Мы восполняем дефицит любви того времени - СЕЙЧАС.
                 </span>
                 </div>
                 <div className={style.item}>
-                    <span>
+                    <TbPoint color='white' size='1.3em'/>
+                    <span className={style.itemText}>
                     Это очень бережная практика. Мягкая и при этом невероятно глубокая. Мы как будто аккуратно вплываем в начало своей истории (зачатие, внутриутробное состояние, рождение, первый год жизни) и мягко трансформируем свой опыт того времени.
                 </span>
                 </div>
                 <div className={style.item}>
-                    <span>
+                    <TbPoint color='white' size='1.3em'/>
+                    <span className={style.itemText}>
                     В этой практике есть мама, любящая безусловно (ведь это простая женщина, с которой до практике вы даже не знакомы, она просто пропускает поток безусловной любви через себя) напитывающая, обеспечивающая безопасность, дающая пищу и остающаяся рядом что бы ни произошло.
                 </span>
                 </div>
                 <div className={style.item}>
-                    <span>
+                    <TbPoint color='white' size='1.3em'/>
+                    <span className={style.itemText}>
                     В этом и есть главный целительный эффект добаюкивания. «МЕНЯ ЛЮБЯТ ПРОСТО ТАК И МАМА ВСЕГДА РЯДОМ, как бы я себя ни вела, МАМА РЯДОМ».
                 </span>
                 </div>
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,6 @@
 import style from './Main.module.css'
 import logo from '../../asset/img/logo.png'
-import foto from '../../asset/img/foto1.png'
+import photoMain from '../../asset/img/foto1.png'
 import {BsFillTelephoneFill} from "react-icons/bs";
 import {FiMail} from "react-icons/fi";
 
@@ -11,20 +11,20 @@ export const Main = () => {
                 <div className={style.logo}>
                     <img src={logo}/>
                 </div>
-                <div className={style.deskriptor}>
+                <div className={style.desGroup}>
                     <span>Мастер по добаюкиванию,</span>
                     <span>переринотальный психолог</span>
                 </div>
-                <div className={style.contactAdress}>
+                <div className={style.contactLocation}>
                     г.Уфа, Улица Аксакова 81/1
                 </div>
                 <div className={style.contactTelEmail}>
                     <div className={style.iconsGroup}>
                         <div>
-                            <BsFillTelephoneFill color='white' size = '1em'/>
+                            <BsFillTelephoneFill color='white' size='1.3em'/>
                         </div>
                         <div>
-                            <FiMail color='white' size = '1em'/>
+                            <FiMail color='white' size='1.3em'/>
                         </div>
                     </div>
                     <div className={style.telEmailGroup}>
@@ -35,40 +35,34 @@ export const Main = () => {
                 </div>
             </div>
             <div className={style.mainGroup}>
-                <div>
+                <div className={style.text}>
                     <div>
-                        <h1>Добаюкивание</h1>
+                        <h1 className={style.h1Title}>ДОБАЮКИВАНИЕ</h1>
+                        <div className={style.spanTitle}>
+                            <span >ПО МЕТОДУ ВИКТОРИИ АВЕРКИЕВОЙ</span>
+                        </div>
                     </div>
-                    <div>
-                        <h3>это телелесная практика, которая</h3>
+                    <div className={style.textHeader}>
+                        <h2 className={style.h2TextHeader}>это телелесная практика, которая</h2>
                     </div>
                     <div className={style.block1}>
-                            <h3>
-                                Даёт возможность перепрожить
-                                своё детство
-                            </h3>
-                        </div>
-                        <div className={style.block1}>
-                            <h3>
-                                Создать опору в себе, заполнить
-                                дефицит материнской любви
-                            </h3>
-                        </div>
-                        <div className={style.block1}>
-                            <h3>
-                                Укрепить доверие миру и другим
-                                людям
-                            </h3>
-                        </div>
-                        <div className={style.block2}>
-                            <h3>
-                                Укрепить доверие миру и другим
-                                людям
-                            </h3>
-                        </div>
+                        <span>Даёт возможность перепрожить</span>
+                        <span>своё детство</span>
+                    </div>
+                    <div className={style.block1}>
+                        <span> Создать опору в себе, заполнить</span>
+                        <span>дефицит материнской любви</span>
+                    </div>
+                    <div className={style.block1}>
+                        <span>Укрепить доверие миру и другим</span>
+                        <span>людям</span>
+                    </div>
+                    <div className={style.block2}>
+                        <span>ЗАПИСАТЬСЯ НА ПРАКТИКУ</span>
+                    </div>
                 </div>
                 <div>
-                    <img src={foto}/>
+                    <img src={photoMain}/>
                 </div>
             </div>
         </div>
