59e430e41221e87d29e3161c1db648e516c2b9e0
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-09-19 16:38:06 +0500

    fix: contacts block add communications

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from "../BlockTwo/BlockTwo.module.css";
+import style from "./Contacts.module.css";
 
 export const Contacts = () => {
     return (
@@ -7,36 +7,56 @@ export const Contacts = () => {
             <div>
                 <div className={style.titlePage}>
                     <h2>
-                        Записаться
+                        Контакты
                     </h2>
                 </div>
-                <div>
-                    <input placeholder='Имя'/>
-                    <input placeholder='Телефон'/>
-                    <button>ЗАПИСАТЬСЯ НА ПРАКТИКУ</button>
+                <div className={style.box}>
+                    <div>
+                        <h3>
+                            Бронирование по телефону и мессенджерах:
+                        </h3>
+                    </div>
+                    <div className={style.boxtel}>
+                        <a className={style.tel} href="tel:+74951234567">
+                            +7 917 345 06 07
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" className={style.communication} href="tel:+74951234567">
+                            <div className={style.box1}>
+                                WhatsApp
+                            </div>
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" className={style.communication} href="tel:+74951234567">
+                            <div className={style.box2}>
+                                Telegram
+                            </div>
+                        </a>
+                    </div>
+                    <div>
+                        <a target="_blank" className={style.communication} href="https://vk.com/jultava">
+                            <div className={style.box3}>
+                                VK
+                            </div>
+                        </a>
+                    </div>
+                    <div>
+                        <div className={style.boxPlaceTitle}>
+                            <h3>
+                                Место проведения
+                            </h3>
+                        </div>
+                        <div className={style.boxPlaceText}>
+                            г.Уфа, Верхнеторговая площадь 4, этаж 6, студия 624
+                        </div>
+                    </div>
                 </div>
+
             </div>
-            <div>
-                <div className={style.titlePage}>
-                    <h2>
-                        Место проведения в г.Уфа
-                    </h2>
-                </div>
-                <div>
-                    <div>
-                        <div>
-                            ТЕЛЕФОН
-                            8 917 345 06 07
-                        </div>
-                        <div>
-                            АДРЕС
-                            г.Уфа, ул.Аксакова
-                        </div>
-                    </div>
-                    <div>
-                        map
-                    </div>
-                </div>
+            <div className={style.boxFooter}>
+                JUJA - студия развития женщины
             </div>
         </div>
     );
